801a557a9d49b9361a7c614e0efe8880849e7d04
riquee <strikedv@hotmail.com>
2021-05-02 20:16:33 -0300

    Exercicio 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import Cars from './Cars';
 
+export const MyContext = createContext();
+
+const INITIAL_STATE = {
+  cars: {
+    red: false,
+    blue: false,
+    yellow: false,
+  },
+};
+
 function App() {
+  const [carsColor, setCars] = useState(INITIAL_STATE);
   return (
-    <Cars />
+    <MyContext.Provider value={{ carsColor, setCars }}>
+      <Cars />
+    </MyContext.Provider>
   );
 }
 
